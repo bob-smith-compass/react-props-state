8d2295ba100e9f6d95084793ad3bcf9a3e7d6c99
David Shams <david.shams@gmail.com>
2019-02-24 10:30:14 -0600

    DEBUG: TypeError: Cannot read property 'names' of null

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       
         <h3>Props</h3>
         <h3>State</h3>
-        <List />
+        <List names={this.state.names}/>
         
       </div>
     );
--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ListElement from '../list-element/ListElement';
 
-export default function List() {
+export default function List(props) {
   return (
     <div style={{border: "solid 1px green", padding: "5px"}}>
       <h3>List</h3>
