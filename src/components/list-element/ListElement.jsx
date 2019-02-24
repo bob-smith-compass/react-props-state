5bac70ef2cbf5f3cf7d9b3c43b916b0be6df26b4
David Shams <david.shams@gmail.com>
2019-02-24 12:18:17 -0600

    bootstrap popper.js and jquery served locally - if no internet

--- src/components/list-element/ListElement.jsx
+++ src/components/list-element/ListElement.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './list-element.css';
 
 export default function ListElement(props) {
-    let handleClick = function() {
-        alert('Clicked');
+    let handleClick = function(selected) {
+        console.log(selected); //Synthetic event
+        return selected;
     }
   return (
     <div style={{border: "solid 1px red", padding: "5px"}}>
     
-      <h3>List Element</h3>
-      <h3 onClick={handleClick} className="clickable">{props.name}</h3>
+      <h3 onClick={handleClick}>List Element</h3>
+      <h3 onClick={(e) => { console.log(this); return this; }} className="clickable">{props.name}</h3>
     </div>
   )
 }
