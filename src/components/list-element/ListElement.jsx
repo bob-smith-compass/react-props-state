5358536bad9bd322849301488691a785b2c8dd1c
David Shams <david.shams@gmail.com>
2019-02-24 11:01:39 -0600

    .clickable styles

--- src/components/list-element/ListElement.jsx
+++ src/components/list-element/ListElement.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import './list-element.css';
 
 export default function ListElement(props) {
+    let handleClick = function() {
+        alert('Clicked');
+    }
   return (
     <div style={{border: "solid 1px red", padding: "5px"}}>
     
       <h3>List Element</h3>
-      <h3>{props.name}</h3>
+      <h3 onClick={handleClick} className="clickable">{props.name}</h3>
     </div>
   )
 }
