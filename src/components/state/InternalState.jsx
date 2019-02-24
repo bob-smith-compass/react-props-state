9799836d41ac60f6e5835851dd7b990fdb811479
David Shams <david.shams@gmail.com>
2019-02-24 13:39:59 -0600

    defaultValue

--- src/components/state/InternalState.jsx
+++ src/components/state/InternalState.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+
+
 export default class InternalState extends Component {
     constructor(props) {
         super(props);
@@ -17,8 +19,8 @@ export default class InternalState extends Component {
       <div>
         <span>{this.state.name}</span> is a <span>{this.state.title}</span>
       </div>
-        Name: <input type="text" value={this.state.name}/>
-        Title: <input type="text" value={this.state.title}/>
+        Name: <input type="text" defaultValue={this.state.name}/>
+        Title: <input type="text" defaultValue={this.state.title}/>
         <button onClick={() => this.setState({title: 'New title'})}>Update</button>
       </div>
     )
