ded4610e1628c0722f894c6374043cdeb1c0773e
David Shams <david.shams@gmail.com>
2019-02-24 13:49:48 -0600

    Force validation PropTypes: Warning: Failed prop type: Invalid prop `name` of type `number` supplied to `SimpleComponent`, expected `string`.

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -5,7 +5,8 @@ import SimpleComponent from '../simple/SimpleComponent';
 export default function List(props) {
   return (
     <div style={{border: "solid 1px green", padding: "5px"}}>
-      <h3>List <SimpleComponent /></h3>
+      <h3>List <SimpleComponent name="Required" /></h3>
+      <h3>List <SimpleComponent name={2} /></h3>
       <SimpleComponent name="David" title="FE Developer" />
       <h4>{props.selected? "Selected":"Nothing selected"}</h4>
       {props.names.map((e, i) => {
