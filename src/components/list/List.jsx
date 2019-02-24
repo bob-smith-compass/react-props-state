085c96833fbc10bbfc887465406e098915f34113
David Shams <david.shams@gmail.com>
2019-02-24 13:47:53 -0600

    name: PropTypes.string.isRequired,

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -6,6 +6,7 @@ export default function List(props) {
   return (
     <div style={{border: "solid 1px green", padding: "5px"}}>
       <h3>List <SimpleComponent /></h3>
+      <SimpleComponent name="David" title="FE Developer" />
       <h4>{props.selected? "Selected":"Nothing selected"}</h4>
       {props.names.map((e, i) => {
           return <div key={i}>
--- src/components/simple/SimpleComponent.jsx
+++ src/components/simple/SimpleComponent.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function SimpleComponent() {
+export default function SimpleComponent(props) {
   return (
     <div>
       {new Date().toISOString()}
+      <div>
+        {props.name} - {props.title}
+      </div>
     </div>
   )
+}
+SimpleComponent.propTypes = {
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string
 }
