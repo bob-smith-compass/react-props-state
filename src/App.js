0c5db1e810ea9658c8ccf8e8304833444feffac3
David Shams <david.shams@gmail.com>
2019-02-24 10:33:33 -0600

    brough dynamic data from <App> to <List>

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import List from './components/list/List';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.names = props.names;
+  }
   render() {
     return (
     <div style={{border: "solid 1px gray", padding: "5px"}}>
       
         <h3>Props</h3>
         <h3>State</h3>
-        <List names={["David", "John", "Bob"]}/>
+        <List names={this.names}/>
         
       </div>
     );
