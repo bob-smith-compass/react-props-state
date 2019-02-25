913f9a00867c6f08280007ff96814d96402b34c2
David Shams <david.shams@gmail.com>
2019-02-24 23:04:08 -0600

    </BrowserRouter>

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import  from 'react-router-dom';
+import  {BrowserRouter, Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -80,7 +80,7 @@ const state = {
 };
 
 // ReactDOM.render(<App names={state.names} selected={state.selected}/>, document.getElementById('root'));
-ReactDOM.render(<App {...state}/>, document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App {...state}/></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
