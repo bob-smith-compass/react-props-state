40e30b29b73ae11eff250f2a9e852282aafde92c
David Shams <david.shams@gmail.com>
2019-02-24 22:14:15 -0600

    [event.target.name]: event.target.value

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -12,6 +12,11 @@ export default class Form extends Component {
     handleSubmit() {
         console.log(`Submitting ${123}`);
     }
+    onFieldChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
     render() {
 
         return (
@@ -19,16 +24,14 @@ export default class Form extends Component {
                 {/* <form onSubmit={this.handleSubmit}> */}
                 <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
-                        <input type="text" defaultValue={'Initial Value'} />
+                        <input onChange={onFieldChange} type="text" defaultValue={'Initial Value'} />
                     </div>
                     <div className="form-group">
-
                         <textarea name="" id="" cols="30" rows="10" defaultValue={"Default value"}>
                             {/* Not like HTML */}
                         </textarea>
                     </div>
                     <div className="form-group">
-
                         <select name="" id="" value={'Two'}>
                             <option value="One">First</option>
                             {/* HTML */}
@@ -38,17 +41,16 @@ export default class Form extends Component {
                         </select>
                     </div>
                     <div className="form-group">
-
                         <label htmlFor="exampleInputEmail1">Email address</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                        <input onChange={onFieldChange} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                         <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
                     <div className="form-group">
                         <label htmlFor="exampleInputPassword1">Password</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                        <input onChange={onFieldChange} type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                     </div>
                     <div className="form-group form-check">
-                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                        <input onChange={onFieldChange} type="checkbox" className="form-check-input" id="exampleCheck1" />
                         <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
