a4654861fae76ea6bb70028c40bc00689d8ccce1
David Shams <david.shams@gmail.com>
2019-02-25 13:39:59 -0600

    question list

--- src/components/question-list/QuestionList.jsx
+++ src/components/question-list/QuestionList.jsx
@@ -2,11 +2,26 @@ import React, { Component } from 'react'
 import Question from '../question/Question';
 
 export default class QuestionList extends Component {
+    constructor(props) {
+        super(props);
+        this.quesions = [
+            {q: "how?", points: 5},
+            {q: "how?", points: 5},
+            {q: "how?", points: 5},
+            {q: "how?", points: 5},
+        ]
+    }
   render() {
     return (
       <div>
         <h3>Questions</h3>
-        <Question />
+        {this.quesions.map((e, i) => {
+            return(
+                <div key={i}>
+                    <Question question={e}/>
+                </div>
+            )
+        })}
       </div>
     )
   }
--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-export default function Question() {
+export default function Question(props) {
   return (
     <div style={{border: "solid 1px gray", padding: "5px"}}>
       <h3>Question</h3>
+      {props.question.q}
     </div>
   )
 }
