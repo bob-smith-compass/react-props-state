8b85fe7f2b7a8e5b5642eccd6d08710b8a8f6f22
David Shams <david.shams@gmail.com>
2019-02-24 21:20:20 -0600

    <select name="" id="" value={'Two'}>

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -19,19 +19,22 @@ export default function Form() {
                 </div>
                 <div className="form-group">
 
-                    <select name="" id="">
+                    <select name="" id="" value={'Two'}>
                         <option value="One">First</option>
-                        <option value="Two">Second</option>
+                        {/* HTML */}
+                        {/* <option value="Two" selected>Second</option> */}
+                        <option value="Two" selected>Second</option>
+                        <option value="Three">Third</option>
                     </select>
                 </div>
                 <div className="form-group">
 
-                    <label for="exampleInputEmail1">Email address</label>
+                    <label htmlFor="exampleInputEmail1">Email address</label>
                     <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group">
-                    <label for="exampleInputPassword1">Password</label>
+                    <label htmlFor="exampleInputPassword1">Password</label>
                     <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
                 </div>
                 <div className="form-group form-check">
