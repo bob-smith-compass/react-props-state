5ab4aacfa8cce6e02aa421af9a23c9e923b42381
David Shams <david.shams@gmail.com>
2019-03-02 10:10:16 -0600

    BUG; bootstrap button doe not take width

--- src/components/question/Question.jsx
+++ src/components/question/Question.jsx
@@ -15,12 +15,12 @@ export default function Question(props) {
             <br />
             <button className="btn btn-primary">{props.question.points}</button>
             <br />
-            <div className="card" style={{width: "18rem;"}}>
+            <div className="card" style={{width: "18 rem;"}}>
                 <img className="card-img-top" src="http://placeimg.com/640/480/animals" alt="Card image cap" />
                     <div className="card-body">
                         <h5 className="card-title">Card title</h5>
                         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">Go somewhere</a>
+                        <a href="#" className="btn btn-primary" style={{minWidth: "6 rem"}}>{props.question.points}</a>
                     </div>
 </div>
             </div>
